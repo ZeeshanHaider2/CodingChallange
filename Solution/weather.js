b3de653c43a52d8206660f284255912af8e02b18
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2019-03-24 01:06:08 +0100

    minor fixes

--- Solution/weather.js
+++ Solution/weather.js
@@ -1,4 +1,4 @@
-function quoteOfDay() {
+quoteOfDay = () => {
   const quoteUrl = "https://api.myjson.com/bins/19l9p.json";
   const getQuotePromise = fetch(quoteUrl);
 
@@ -7,11 +7,11 @@ function quoteOfDay() {
       //restApi has been converted into JS object, which is saved in allQuotes. We will shuffle the allQuotes i.e.objects by using math.random method to get a single random Js object.
       let shuffeledQuotes =
         allQuotes[Math.floor(Math.random() * allQuotes.length)];
-      document.getElementById("quote").innerHTML = shuffeledQuotes.quote;
-      document.getElementById("author").innerHTML = shuffeledQuotes.author;
+      let y = '"' + shuffeledQuotes.quote + '" - ' + shuffeledQuotes.author;
+      document.getElementById("quote").innerHTML = y;
     });
   });
-}
+};
 
 const ourButton = document.getElementById("quote-button");
 ourButton.addEventListener("click", quoteOfDay);
